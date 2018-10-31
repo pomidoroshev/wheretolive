9106a6d32714737ff0b2d37c11d88d34aa09ae87
Dmitry Doroshev <r.schweppes@ya.ru>
2018-10-31 09:07:43 +0300

    Add controls

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-const MAX_DURATION = 40 * 60;
 const CENTER = [55.753855, 37.615931];
 const RADIUS = 0.01
 const R_MIN = 0, R_MAX = 10
+const DEFAULT_DURATION = 30
+
+let coords;
 
 function fix(x, prec) {
     return parseFloat(x.toFixed(prec));
@@ -39,7 +41,8 @@ function getDuration(fromCoords, toCoords, routingMode = 'masstransit') {
 }
 
 function checkDuration(duration, gap = 60) {
-    let delta = MAX_DURATION - duration
+    let maxDuration = parseInt($('#duration').val()) * 60;
+    let delta = maxDuration - duration
     let absDelta = Math.abs(delta)
 
     if (absDelta > gap) {
@@ -79,11 +82,7 @@ async function getAppropriatePoint(center, angle, R, Rmin, Rmax, map) {
     return await getAppropriatePoint(center, angle, Rtmp, Rmin, Rmax, map)
 }
 
-async function init() {
-    let myMap = new ymaps.Map('map', {
-        center: CENTER,
-        zoom: 11
-    });
+async function drawArea(point, myMap) {
     let R = mean(R_MIN, R_MAX)
     let coords
     let polyline = new ymaps.Polyline([], {}, {
@@ -91,20 +90,71 @@ async function init() {
         strokeWidth: 3
     })
 
-    myMap.geoObjects.add(new ymaps.Placemark(CENTER, {}, {
-        preset: 'islands#redCircleDotIcon'
-    }));
-
     myMap.geoObjects.add(polyline)
     for (let i = 0; i < 36; i++) {
-        polyline.geometry.insert(polyline.geometry.getLength(), CENTER)
+        polyline.geometry.insert(polyline.geometry.getLength(), point)
     }
     for (let angle = 0, i = 0; angle <= 360; angle += 10, i++) {
         setTimeout(async function () {
-            ({coords, R} = await getAppropriatePoint(CENTER, angle, R, R_MIN, R_MAX, myMap));
+            ({coords, R} = await getAppropriatePoint(point, angle, R, R_MIN, R_MAX, myMap));
             polyline.geometry.set(i, coords)
         }, 0)
     }
+}
+
+function init() {
+    let myMap = new ymaps.Map('map', {
+        center: CENTER,
+        zoom: 11
+    });
+
+    let CustomControlClass = function (options) {
+        CustomControlClass.superclass.constructor.call(this, options);
+        this._$content = null;
+        this._geocoderDeferred = null;
+    };
+
+    ymaps.util.augment(CustomControlClass, ymaps.collection.Item, {
+        onAddToMap: function (map) {
+            CustomControlClass.superclass.onAddToMap.call(this, map);
+            this.getParent().getChildElement(this).then(this._onGetChildElement, this);
+        },
+
+        _onGetChildElement: function (parentDomContainer) {
+            let html = '<div class="duration">' +
+                         '<label for="duration">Время в пути (мин):</label>' +
+                         '<input type="number" value="' + DEFAULT_DURATION + '" id="duration">' +
+                         '<button class="doit">Нарисовать</button>' +
+                         '<div class="hint">Сначала нажмите на карту, чтобы поставить точку</div>' +
+                       '</div>';
+            this._$content = $(html).appendTo(parentDomContainer);
+            $('.doit').on('click', function() {
+                drawArea(coords, myMap);
+            });
+        },
+    });
+    var customControl = new CustomControlClass();
+    myMap.controls.add(customControl, {
+        float: 'none',
+        position: {
+            top: 10,
+            left: 10
+        }
+    });
+
+    let placemark;
+
+    myMap.events.add('click', function (e) {
+        coords = e.get('coords');
+        if (placemark) {
+            myMap.geoObjects.remove(placemark)
+        }
+        placemark = new ymaps.Placemark(coords, {}, {
+            preset: 'islands#redCircleDotIcon'
+        })
+        myMap.geoObjects.add(placemark)
+        $('.hint').hide()
+    });
 }
 
 window.ymaps.ready(init)
